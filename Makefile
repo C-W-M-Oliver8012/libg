8e5c3281a3fc0d17ac5217b50f6f6366d03a54bd
Caden Miller <mcaden966@gmail.com>
2021-05-04 20:21:25 -0500

    deleted memory library and number library

--- Makefile
+++ Makefile
@@ -3,13 +3,12 @@ lib_flags:= -std=c17 -Wall -Wextra -Werror
 debug_flags:= -std=c17 -Wall -Wextra -Werror -g
 release_flags:= -std=c17 -Wall -Wextra -Werror -static
 
-library: dstring.c ivec.c dvec.c memg.c
+library: dstring.c ivec.c dvec.c
 	$(cc) $(lib_flags) -c dstring.c
 	$(cc) $(lib_flags) -c ivec.c
 	$(cc) $(lib_flags) -c dvec.c
-	$(cc) $(lib_flags) -c memg.c
-	ar rcs libg.a dstring.o ivec.o dvec.o memg.o
-	rm dstring.o ivec.o dvec.o memg.o
+	ar rcs libg.a dstring.o ivec.o dvec.o
+	rm dstring.o ivec.o dvec.o
 
 debug: test.c
 	$(cc) $(debug_flags) test.c libg.a -o test
--- test.c
+++ test.c
@@ -1,34 +1,19 @@
 #include "libg.h"
 
-#define TEST_SIZE 15
-
 int main(void) {
-	Mem_Hash mem_hash;
-	init_mem_hash(&mem_hash);
+	bool is_error = false;
 
-	int n[TEST_SIZE];
-	for (int i = 0; i < TEST_SIZE; i++) {
-		add_mem_item_to_mem_hash(&mem_hash, &n[i]);
+	Dstring input;
+	if (init_dstring(&input) == true) {
+		is_error = true;
 	}
-	print_mem_hash(&mem_hash);
 
-	printf("\n");
-	for (int i = 0; i < TEST_SIZE; i++) {
-		if (contains_ptr_mem_hash(&mem_hash, &n[i]) == true) {
-			printf("true\n");
-		} else {
-			printf("false\n");
+	while (strcmp(input.string, "quit")) {
+		if (input.get_input(&input) == true) {
+			is_error = true;
 		}
+		printf("%s\n\n", input.string);
 	}
-	
-	int d = 45;
-	if (contains_ptr_mem_hash(&mem_hash, &d) == true) {
-		printf("true\n");
-	} else {
-		printf("false\n");
-	}
-
-	delete_mem_hash(mem_hash.hash_map, mem_hash.len);
-
-	return 0;
+	input.clear(&input);
+	return is_error;
 }
